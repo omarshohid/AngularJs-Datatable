de66a9e1faa6f9a159a951de75905634af952f87
Shohid <omarshohid@gmail.com>
2021-01-17 15:26:27 +0600

    init

---
+++ MvcNGDatatable/MvcNGDatatable/Controllers/HomeController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Linq.Dynamic;
+
+namespace MvcNGDatatable.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult getData()
+        {
+            //Datatable parameter
+            var draw = Request.Form.GetValues("draw").FirstOrDefault();
+
+            List<ColumnSearch> lstColumnSearch = new List<ColumnSearch>();
+            //Individual Column Filtering.
+            foreach (string key in Request.Form)
+            {
+                if (key.StartsWith("columns") && key.Contains("[search][value]"))
+                {
+                    var columnName = Request.Form.GetValues(key.Replace("[search][value]","")+ "[name]").FirstOrDefault();
+                    var searchText = Request.Form.GetValues(key).FirstOrDefault();
+                    if (!string.IsNullOrEmpty(searchText))
+                    {
+                        var search = new ColumnSearch() { columnName = columnName, searchText = searchText };
+                        lstColumnSearch.Add(search);
+                    }
+                }
+            }
+
+
+
+            //paging parameter
+            var start = Request.Form.GetValues("start").FirstOrDefault();
+            var length = Request.Form.GetValues("length").FirstOrDefault();
+            //sorting parameter
+            var sortColumn = Request.Form.GetValues("columns[" + Request.Form.GetValues("order[0][column]").FirstOrDefault() + "][name]").FirstOrDefault();
+            var sortColumnDir = Request.Form.GetValues("order[0][dir]").FirstOrDefault();
+            //filter parameter
+            var searchValue = Request.Form.GetValues("search[value]").FirstOrDefault();
+
+
+
+            List<SparePartsItem> allCustomer = new List<SparePartsItem>();
+            int pageSize = length != null ? Convert.ToInt32(length) : 0;
+            int skip = start != null ? Convert.ToInt32(start) : 0;
+            int recordsTotal = 0;
+
+            //Database query
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                var v = (from a in dc.SparePartsItems select a);
+
+                //search
+                if (!string.IsNullOrEmpty(searchValue))
+                {
+                    v = v.Where(a => 
+                        a.PartsName.Contains(searchValue) ||
+                        a.PartsNo.Contains(searchValue) ||
+                        //a.Quantity.ToString().Contains(searchValue) ||
+                        //a.Amount.ToString().Contains(searchValue) || 
+                        a.SheetNo.Contains(searchValue) ||
+                        a.Supplier.Contains(searchValue)
+                        );
+                }
+                //string columnFilter = "";
+                //column search
+                foreach (var item in lstColumnSearch)
+                {
+                    var searchtext = item.searchText.Split(',');
+                    foreach (var text in searchtext)
+                    {
+                        var func = $"{item.columnName}.ToString().Contains(\"{text}\")";
+                        v = (IQueryable<SparePartsItem>)v.ToList().Where(func);
+                    }                   
+                }
+
+                //sort
+                if (!(string.IsNullOrEmpty(sortColumn) && string.IsNullOrEmpty(sortColumnDir)))
+                {
+                    //for make sort simpler we will add Syste.Linq.Dynamic reference
+                    v = v.OrderBy(sortColumn + " " + sortColumnDir);
+                }
+
+                recordsTotal = v.Count();
+                allCustomer = v.Skip(skip).Take(pageSize).ToList();
+            }
+
+            return Json(new { draw = draw, recordsFiltered = recordsTotal, recordsTotal = recordsTotal, data = allCustomer });
+        }
+	}
+
+    public class ColumnSearch
+    {
+        public string columnName { get; set; }
+        public string searchText { get; set; }
+    }
+}
+
---
+++ MvcNGDatatable/MvcNGDatatable/Scripts/myApp.js
@@ -0,0 +1,138 @@
+﻿var app = angular.module('MyApp', ['datatables', 'datatables.buttons']);
+app.controller('homeCtrl', ['$scope', '$http', 'DTOptionsBuilder', 'DTColumnBuilder',
+    function ($scope, $http, DTOptionsBuilder, DTColumnBuilder) {
+
+        $sentimentArray = ['Positive', 'Neutral', 'Negative'];
+        $sourceArray = ['Twitter', 'Facebook', 'Instagram', 'Reddit', 'Youtube', 'Forum', 'Blog', 'Comment', 'Web'];
+
+        $scope.dtColumns = [
+            //here We will add .withOption('name','column_name') for send column name to the server 
+            DTColumnBuilder.newColumn("Id", "ID").withOption('name', 'Id'),
+            DTColumnBuilder.newColumn("PartsName", "Parts Name").withOption('name', 'PartsName'),
+            DTColumnBuilder.newColumn("PartsNo", "Parts No").withOption('name', 'PartsNo'),
+            DTColumnBuilder.newColumn("Quantity", "Quantity").withOption('name', 'Quantity'),
+            DTColumnBuilder.newColumn("SheetNo", "Sheet No").withOption('name', 'SheetNo'),
+            DTColumnBuilder.newColumn("UnitPrice", "Unit Price").withOption('name', 'UnitPrice'),
+            DTColumnBuilder.newColumn("Amount", "Amount").withOption('name', 'Amount'),
+            DTColumnBuilder.newColumn("Supplier", "Supplier").withOption('name', 'Supplier')
+        ]
+
+        $scope.dtOptions = DTOptionsBuilder.newOptions().withOption('ajax', {
+            dataSrc: "data",
+            url: "/home/getdata",
+            type: "POST"
+        })
+            .withOption('processing', true) //for show progress bar
+            .withDOM('lBfrtip')
+            .withOption('serverSide', true) // for server side processing
+            .withPaginationType('full_numbers') // for get full pagination options // first / last / prev / next and page numbers
+            .withDisplayLength(10) // Page size
+            .withOption('aaSorting', [0, 'asc'])  // for default sorting column // here 0 means first column       
+            .withButtons([
+                //{
+                //    extend: 'copy',
+                //    text: '<i class="fa fa-files-o"></i> Copy',
+                //    titleAttr: 'Copy'
+                //},
+                //{
+                //    extend: 'print',
+                //    text: '<i class="fa fa-print" aria-hidden="true"></i> Print',
+                //    titleAttr: 'Print'
+                //},
+                {
+                    extend: 'excel',
+                    text: '<i class="fa fa-file-text-o"></i> Excel',
+                    titleAttr: 'Excel'
+                }
+            ])
+            .withOption('initComplete', function () {
+                setTimeout(function () {
+                    $scope.setBuyerListFilter()
+                }                   
+                 ,1000);
+            });
+        //.initComplete(function () {
+        //    $scope.setBuyerListFilter();
+        //});
+
+
+
+
+
+        $scope.setBuyerListFilter = function () {
+            //$('#pBuyer tfoot th').each(function () {
+            //    var title = $(this).text();
+            //    //console.log($(this).attr('type'));
+            //    if ($(this).attr('type') == 'text') {
+            //        $(this).html('<input type="text" class="table-filter" placeholder="Search ' + title + '" />');
+            //    }
+            //    else if ($(this).attr('type') == 'text') {
+            //        $(this).html('<input type="text" class="table-filter" placeholder="Search ' + title + '" />');
+            //    }
+            //});
+
+            var table = $('#entry-grid').DataTable();
+            table.columns().every(function () {
+                var column = this;
+                if ($(this.header()).attr('type') == 'text') {
+                    $('<input type="text" class="table-filter" placeholder="Search" />')
+                        .appendTo($(column.footer()).empty())
+                        .on('keyup change', function () {
+                            var val = $.fn.dataTable.util.escapeRegex(
+                                $(this).val()
+                            );
+                            column.search(val).draw();
+                        });
+                }
+                else if ($(this.header()).attr('type') == 'sentiment-select') {
+                    var select = $('<select multiple class="select" style="width: 100%;"><option value=""></option></select>')
+                        .appendTo($(column.footer()).empty())
+                        .on('change', function () {
+                            //console.log($(this).val());
+                            var val = '';
+                            if ($(this).val() != null) {
+                                var val = $(this).val().join('|');
+                            }
+                            //column.search(val.length > 0 ? '^(' + val + ')$' : '', true, false).draw();
+                            column.search(val.length > 0 ? '' + val + '' : '', true, false).draw();
+                        });
+                    $sentimentArray.forEach(function (d, j) {
+                        select.append('<option value="' + d + '">' + d + '</option>')
+                    });
+
+                }
+                else if ($(this.header()).attr('type') == 'source-select') {
+                    var select = $('<select multiple class="select" style="width: 100%;"><option value=""></option></select>')
+                        .appendTo($(column.footer()).empty())
+                        .on('change', function () {
+                            var val = '';
+                            if ($(this).val() != null) {
+                                var val = $(this).val().join('|');
+                            }
+                            //column.search(val.length > 0 ? '^(' + val + ')$' : '', true, false).draw();
+                            column.search(val.length > 0 ? '' + val + '' : '', true, false).draw();
+                        });
+                    $sourceArray.forEach(function (d, j) {
+                        select.append('<option value="' + d + '">' + d + '</option>')
+                    });
+                }
+                else if ($(this.header()).attr('type') == 'select') {
+                    var select = $('<select multiple class="select" style="width: 100%;"><option value=""></option></select2>')
+                        .appendTo($(column.footer()).empty())
+                        .on('change', function () {
+                            var val = '';
+                            if ($(this).val() != null) {
+                                var val = $(this).val().join('|');
+                            }
+                            //column.search(val.length > 0 ? '^(' + val + ')$' : '', true, false).draw();
+                            column.search(val.length > 0 ? '' + val + '' : '', true, false).draw();
+                        });
+                    column.data().unique().sort().each(function (d, j) {
+                        select.append('<option value="' + d + '">' + d + '</option>')
+                    });
+                }
+            });
+            $('.select').select2();
+        }
+
+    }])
